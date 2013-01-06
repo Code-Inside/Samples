f6b8ee4700474805a0a51bb3308eef0b64c280d5
Robert Muehsig <robert.muehsig@code-developer.de>
2013-01-06 23:34:53 +0100

    Logging with Trace & Debug
    http://code-inside.de/blog/2013/01/06/loggen-mit-trace-debug-achtung-nach-dem-bauen-knnten-die-logausschriften-verschwinden/

---
+++ 2013/TraceAndDebug/TraceAndDebug/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraceAndDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Foobar();
+
+            Trace.WriteLine("Hello Tracing-World!");
+
+            Debug.WriteLine("Hello Debug-World!");
+        }
+
+        [Conditional("DEBUG")]
+        public static void Foobar()
+        {
+            Console.WriteLine("Foobar with DEBUG");
+        }
+
+    }
+}
