6a05f20212ad657e595645940e647587970d6bd6
Robert Muehsig <robert.muehsig@code-developer.de>
2013-09-26 23:05:42 +0200

    owin razor webapi blogpost

---
+++ 2013/RazorWebApiOwin/RazorWebApiOwin/Api/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using WebApiContrib.Formatting.Html;
+
+namespace RazorWebApiOwin.Api
+{
+    // http://weblogs.asp.net/fredriknormen/archive/2012/07/17/asp-net-web-api-and-using-razor-the-next-step.aspx
+    public class HomeController : ApiController
+    {
+        public Value GetValues()
+        {
+            // Option via return type: return new View("Home", null);
+            return new Value() { Numbers = new int[] { 1, 2, 3 } };
+        }
+    }
+
+    [View("Home")]
+    public class Value
+    {
+        public int[] Numbers { get; set; }
+    }
+}
---
+++ 2013/RazorWebApiOwin/RazorWebApiOwin/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using WebApiContrib.Formatting.Html.Configuration;
+using WebApiContrib.Formatting.Html.Formatters;
+using WebApiContrib.Formatting.Html.Locators;
+using WebApiContrib.Formatting.Razor;
+
+[assembly: OwinStartup(typeof(RazorWebApiOwin.Startup))]
+namespace RazorWebApiOwin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.Formatters.Add(new HtmlMediaTypeViewFormatter());
+
+            GlobalViews.DefaultViewParser = new RazorViewParser();
+            GlobalViews.DefaultViewLocator = new RazorViewLocator();
+
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("default", "{controller}");
+            config.Formatters.Add(new HtmlMediaTypeViewFormatter());
+
+            app.UseWebApi(config);
+
+            app.UseWelcomePage();
+        }
+    }
+}
