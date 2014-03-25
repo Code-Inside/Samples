899d802b17c905d87effca3f7e458e598da54b8f
Robert Muehsig <robert.muehsig@code-developer.de>
2014-03-25 22:39:38 +0100

    fontawesomedemo

---
+++ 2014/FontawesomeDemo/FontawesomeDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FontawesomeDemo.MvcApplication" Language="C#" %>
---
+++ 2014/FontawesomeDemo/FontawesomeDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FontawesomeDemo.Startup))]
+namespace FontawesomeDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
