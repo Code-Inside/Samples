9fdf3fce93e96916711248ef689326f322f67c02
Robert Muehsig <robert.muehsig@code-developer.de>
2014-04-29 23:51:07 +0200

    twitter retweet counter with unofficial api

---
+++ 2014/RetweetCounter/TwitterRetweets/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterRetweets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://urls.api.twitter.com/1/urls/count.json?url=http://blog.codeinside.eu/2014/04/26/fix-excel-com-exception-code-2147467259-exception-from-hresult-0x80028018/#comments";
+
+            // Be carefull with this code - use async/await - 
+            // pure demo code inside a console application
+
+            var client = new HttpClient();
+            var result = client.GetAsync(url).Result;
+
+            Console.WriteLine(result.Content.ReadAsStringAsync().Result);
+            Console.ReadLine();
+        }
+    }
+}
