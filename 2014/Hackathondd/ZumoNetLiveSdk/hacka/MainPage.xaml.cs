dada26270cd0b09ddb0d65e1ca2efa5c456c8aa1
Robert Muehsig <robert.muehsig@code-developer.de>
2014-05-23 23:57:26 +0200

    Hackathon DD - Argh...

---
+++ 2014/Hackathondd/ZumoNetLiveSdk/hacka/MainPage.xaml.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.Live;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace hacka
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+
+        [JsonProperty(PropertyName = "complete")]
+        public bool Complete { get; set; }
+    }
+
+    public sealed partial class MainPage : Page
+    {
+        private MobileServiceCollection<TodoItem, TodoItem> items;
+        private IMobileServiceTable<TodoItem> todoTable = App.MobileService.GetTable<TodoItem>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void InsertTodoItem(TodoItem todoItem)
+        {
+            // This code inserts a new TodoItem into the database. When the operation completes
+            // and Mobile Services has assigned an Id, the item is added to the CollectionView
+            await todoTable.InsertAsync(todoItem);
+            items.Add(todoItem);
+        }
+
+        private async void RefreshTodoItems()
+        {
+            MobileServiceInvalidOperationException exception = null;
+            try
+            {
+                // This code refreshes the entries in the list view by querying the TodoItems table.
+                // The query excludes completed TodoItems
+                items = await todoTable
+                    .Where(todoItem => todoItem.Complete == false)
+                    .ToCollectionAsync();
+                this.ButtonSave.IsEnabled = true;
+            }
+            catch (MobileServiceInvalidOperationException e)
+            {
+                exception = e;
+            }
+
+            if (exception != null)
+            {
+                await new MessageDialog(exception.Message, "Error loading items").ShowAsync();
+            }
+            else
+            {
+                ListItems.ItemsSource = items;
+            }
+        }
+
+        private async void UpdateCheckedTodoItem(TodoItem item)
+        {
+            // This code takes a freshly completed TodoItem and updates the database. When the MobileService 
+            // responds, the item is removed from the list 
+            await todoTable.UpdateAsync(item);
+            items.Remove(item);
+        }
+
+        private void ButtonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshTodoItems();
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            var todoItem = new TodoItem { Text = TextInput.Text };
+            InsertTodoItem(todoItem);
+        }
+
+        private void CheckBoxComplete_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            TodoItem item = cb.DataContext as TodoItem;
+            UpdateCheckedTodoItem(item);
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await Authenticate();
+            RefreshTodoItems();
+        }
+
+        private LiveConnectSession session;
+        private async System.Threading.Tasks.Task Authenticate()
+        {
+            LiveAuthClient liveIdClient = new LiveAuthClient("URL...");
+
+            while (session == null)
+            {
+                // Force a logout to make it easier to test with multiple Microsoft Accounts
+                if (liveIdClient.CanLogout)
+                    liveIdClient.Logout();
+
+                LiveLoginResult result = await liveIdClient.LoginAsync(new[] { "wl.basic" });
+                if (result.Status == LiveConnectSessionStatus.Connected)
+                {
+                    session = result.Session;
+                    LiveConnectClient client = new LiveConnectClient(result.Session);
+                    LiveOperationResult meResult = await client.GetAsync("me");
+                    MobileServiceUser loginResult = await App.MobileService
+                        .LoginWithMicrosoftAccountAsync(result.Session.AuthenticationToken);
+
+                    string title = string.Format("Welcome {0}!", meResult.Result["first_name"]);
+                    var message = string.Format("You are now logged in - {0}", loginResult.UserId);
+                    var dialog = new MessageDialog(message, title);
+                    dialog.Commands.Add(new UICommand("OK"));
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    session = null;
+                    var dialog = new MessageDialog("You must log in.", "Login Required");
+                    dialog.Commands.Add(new UICommand("OK"));
+                    await dialog.ShowAsync();
+                }
+            }
+        }
+    }
+}
---
+++ 2014/Hackathondd/ZumoNetLiveSdk/hackaService/App_Start/WebApiConfig.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Mobile.Service;
+using hackaService.DataObjects;
+using hackaService.Models;
+
+namespace hackaService
+{
+    public static class WebApiConfig
+    {
+        public static void Register()
+        {
+            // Use this class to set configuration options for your mobile service
+            ConfigOptions options = new ConfigOptions();
+
+            // Use this class to set WebAPI configuration options
+            HttpConfiguration config = ServiceConfig.Initialize(new ConfigBuilder(options));
+
+            // To display errors in the browser during development, uncomment the following
+            // line. Comment it out again when you deploy your service for production use.
+            // config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+            
+            Database.SetInitializer(new hackaInitializer());
+        }
+    }
+
+    public class hackaInitializer : DropCreateDatabaseIfModelChanges<hackaContext>
+    {
+        protected override void Seed(hackaContext context)
+        {
+            List<TodoItem> todoItems = new List<TodoItem>
+            {
+                new TodoItem { Id = "1", Text = "First item", Complete = false },
+                new TodoItem { Id = "2", Text = "Second item", Complete = false },
+            };
+
+            foreach (TodoItem todoItem in todoItems)
+            {
+                context.Set<TodoItem>().Add(todoItem);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
+
