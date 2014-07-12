c35a5b0a644d7b8dad35c7d0711760229bceb28f
Robert Muehsig <robert.muehsig@code-developer.de>
2014-07-12 16:57:44 +0200

    DWX-Owin Sample

---
+++ 2014/DWX-Owin/HelloOwinWebWithMiddleWare/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+namespace HelloOwinWebWithMiddleWare
+{
+    [assembly: OwinStartup(typeof(HelloOwinWebWithMiddleWare.Startup))]
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use<LoggerMiddleware>();
+
+            app.UseHandlerAsync((req, res) =>
+            {
+                res.ContentType = "text/plain";
+                return res.WriteAsync("Hello World!");
+            });
+        }
+    }
+
+    public class LoggerMiddleware : OwinMiddleware
+    {
+        public LoggerMiddleware(OwinMiddleware next)
+            : base(next)
+        {
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            Trace.TraceInformation("Middleware begin");
+            await this.Next.Invoke(context);
+            Trace.TraceInformation("Middleware end");
+        }
+    }
+}
