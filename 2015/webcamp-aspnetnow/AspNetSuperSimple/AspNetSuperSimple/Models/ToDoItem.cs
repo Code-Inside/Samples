2a2009ca4ff0e4422d02dfa88d5117f3570bf242
Robert Muehsig <robert.muehsig@code-developer.de>
2015-05-18 21:06:24 +0200

    init sample

---
+++ 2015/webcamp-aspnetnow/AspNetSuperSimple/AspNetSuperSimple/Models/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetSuperSimple.Models
+{
+    public class ToDoItem
+    {
+        public Guid Id { get; set; }
+        public string Task { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
