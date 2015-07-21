8ac70cc25e7c2c9e499192590ac05fd67fcb500b
robertmuehsig <robert.muehsig@code-developer.de>
2015-07-21 14:47:18 +0200

    jumplists + wpf

---
+++ 2015/Jumplists/Jumplist-Sample/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Shell;
+
+namespace Jumplist_Sample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void JumpList_JumpItemsRejected(object sender, JumpItemsRejectedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void JumpList_JumpItemsRemovedByUser(object sender, JumpItemsRemovedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 2015/Jumplists/Jumplist-Sample/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Shell;
+
+namespace Jumplist_Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var di = new DirectoryInfo("C://");
+            var jt = new JumpTask
+            {
+                ApplicationPath = "C:\\Windows\\notepad.exe",
+                Arguments = "C://",
+                Description = "Run at " + "C://",
+                Title = di.Name,
+                CustomCategory = "Hello World"
+            };
+
+            JumpList.AddToRecentCategory(jt);
+        }
+    }
+}
