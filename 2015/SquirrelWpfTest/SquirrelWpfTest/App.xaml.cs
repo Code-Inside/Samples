fe2f68d6e6d72caa154c46b24fccca64c1aca13a
Robert Muehsig <robert.muehsig@codeinside.eu>
2015-09-02 12:01:10 +0200

    squirrelwpf

---
+++ 2015/SquirrelWpfTest/SquirrelWpfTest/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Squirrel;
+
+namespace SquirrelWpfTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            try
+            {
+                using (var mgr = new UpdateManager(ConfigurationManager.AppSettings["UpdatePath"]))
+                {
+                    // Note, in most of these scenarios, the app exits after this method
+                    // completes!
+                    
+                    SquirrelAwareApp.HandleEvents(
+                        onInitialInstall: v => mgr.CreateShortcutForThisExe(),
+                        onAppUpdate: v => mgr.CreateShortcutForThisExe(),
+                        onAppUninstall: v => mgr.RemoveShortcutForThisExe());
+                }
+            }
+            catch (Exception)
+            {
+                
+            }
+           
+        }
+    }
+}
---
+++ 2015/SquirrelWpfTest/SquirrelWpfTest/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Squirrel;
+
+namespace SquirrelWpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       private async void ButtonUpdateQuestion_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var mgr = new UpdateManager(ConfigurationManager.AppSettings["UpdatePath"]))
+                {
+                    var release = await mgr.CheckForUpdate();
+                    this.UpdateQuestionResult.Text = "Possible Update: " + release.FutureReleaseEntry.Version.ToString();
+                }
+            }
+            catch (Exception test)
+            {
+                this.UpdateQuestionResult.Text = test.Message;
+            }
+        }
+
+        private async void ButtonUpdateDo_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var mgr = new UpdateManager(ConfigurationManager.AppSettings["UpdatePath"]))
+                {
+                    var release = await mgr.UpdateApp();
+                   
+                    this.UpdateDoResult.Text = release.EntryAsString;
+                }
+            }
+            catch (Exception test)
+            {
+                this.UpdateDoResult.Text = "Updated to: " + test.Message;
+            }
+        }
+    }
+}
