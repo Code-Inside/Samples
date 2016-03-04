917d5b7be148c181cb2b50ee3da4392833d358af
Robert Muehsig <robert.muehsig@codeinside.eu>
2016-03-04 23:47:16 +0100

    xsd autocompletion for elements

--- 2016/XmlIntelliSense/XmlIntelliSense.App/MainWindow.xaml.cs
+++ 2016/XmlIntelliSense/XmlIntelliSense.App/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.XPath;
+using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
 using XmlIntelliSense.App.SharpDevelopXmlEditor;
+using XmlIntelliSense.App.XSemmel;
 
 namespace XmlIntelliSense.App
 {
@@ -21,6 +30,7 @@ namespace XmlIntelliSense.App
     /// </summary>
     public partial class MainWindow : Window
     {
+        public XDocument LastValidXDocument { get; set; }
         public MainWindow()
         {
             InitializeComponent();
@@ -32,68 +42,77 @@ namespace XmlIntelliSense.App
 
         private void TextArea_TextEntered(object sender, TextCompositionEventArgs e)
         {
+            var _editor = textEditor;
+
             try
             {
+                var lastXDoc = XDocument.Parse(_editor.Text);
+                LastValidXDocument = lastXDoc;
+            }
+            catch (Exception exc)
+            {
+                // can happen...
+            }
+
+            try
+            {
+
                 switch (e.Text)
                 {
                     case ">":
-                    {
-                        //auto-insert closing element
-                        int offset = textEditor.CaretOffset;
-                        var test =  XmlParser.GetActiveElementStartPath(textEditor.Text, offset - 1);
-                        var s = test.Elements.GetLast().Name;
-                        if (!string.IsNullOrWhiteSpace(s) && "!--" != s)
                         {
-                            //if (!XParser.IsClosingElement(_editor.Text, offset - 1, s))
-                            //{
-                                string endElement = "</" + s + ">";
-                                var rightOfCursor =
-                                    textEditor.Text.Substring(offset,
-                                        Math.Max(0, Math.Min(endElement.Length + 50, textEditor.Text.Length) - offset - 1))
-                                        .TrimStart();
-                                if (!rightOfCursor.StartsWith(endElement))
+                            //auto-insert closing element
+                            int offset = _editor.CaretOffset;
+                            string s = XParser.GetElementAtCursor(_editor.Text, offset - 1);
+                            if (!string.IsNullOrWhiteSpace(s) && "!--" != s)
+                            {
+                                if (!XParser.IsClosingElement(_editor.Text, offset - 1, s))
                                 {
-                                    textEditor.TextArea.Document.Insert(offset, endElement);
-                                    textEditor.CaretOffset = offset;
+                                    string endElement = "</" + s + ">";
+                                    var rightOfCursor = _editor.Text.Substring(offset, Math.Max(0, Math.Min(endElement.Length + 50, _editor.Text.Length) - offset - 1)).TrimStart();
+                                    if (!rightOfCursor.StartsWith(endElement))
+                                    {
+                                        _editor.TextArea.Document.Insert(offset, endElement);
+                                        _editor.CaretOffset = offset;
+                                    }
                                 }
-                            //}
+                            }
+                            break;
                         }
-                        break;
-                    }
                     case "/":
                         {
                             //insert name of closing element
-                            int offset = textEditor.CaretOffset;
-                            if (offset > 1 && textEditor.Text[offset - 2] == '<')
+                            int offset = _editor.CaretOffset;
+                            if (offset > 1 && _editor.Text[offset - 2] == '<')
                             {
                                 //expand to closing tag
-                                string s = XmlParser.GetActiveElementStartPath(textEditor.Text, offset - 1).Elements.GetLast().Name;
-                                //if (!string.IsNullOrEmpty(s))
-                                //{
-                                //    showCompletion(new List<ICompletionData>
-                                //    {
-                                //        new MyCompletionData(s + ">")
-                                //    });
-                                //    return true;
-                                //}
+                                string s = XParser.GetParentElementAtCursor(_editor.Text, offset - 1);
+                                if (!string.IsNullOrEmpty(s))
+                                {
+                                    //   showCompletion(new List<ICompletionData>
+                                    //   {
+                                    //       new MyCompletionData(s + ">")
+                                    //});
+                                    //  return true;
+                                }
                             }
-                            if (textEditor.Text.Length > offset + 2 && textEditor.Text[offset] == '>')
+                            if (_editor.Text.Length > offset + 2 && _editor.Text[offset] == '>')
                             {
                                 //remove closing tag if exist
-                                string s = XmlParser.GetActiveElementStartPath(textEditor.Text, offset - 1).Elements.GetLast().Name;
+                                string s = XParser.GetElementAtCursor(_editor.Text, offset - 1);
                                 if (!string.IsNullOrWhiteSpace(s))
                                 {
                                     //search closing end tag. Element must be empty (whitespace allowed)  
                                     //"<hallo>  </hallo>" --> enter '/' --> "<hallo/>  "
                                     string expectedEndTag = "</" + s + ">";
-                                    for (int i = offset + 1; i < textEditor.Text.Length - expectedEndTag.Length + 1; i++)
+                                    for (int i = offset + 1; i < _editor.Text.Length - expectedEndTag.Length + 1; i++)
                                     {
-                                        if (!char.IsWhiteSpace(textEditor.Text[i]))
+                                        if (!char.IsWhiteSpace(_editor.Text[i]))
                                         {
-                                            if (textEditor.Text.Substring(i, expectedEndTag.Length) == expectedEndTag)
+                                            if (_editor.Text.Substring(i, expectedEndTag.Length) == expectedEndTag)
                                             {
                                                 //remove already existing endTag
-                                                textEditor.Document.Remove(i, expectedEndTag.Length);
+                                                _editor.Document.Remove(i, expectedEndTag.Length);
                                             }
                                             break;
                                         }
@@ -102,11 +121,29 @@ namespace XmlIntelliSense.App
                             }
                             break;
                         }
+                    case "<":
+                        var test = XmlParser.GetParentElementPath(_editor.Text);
+                        var autocompletelist = ProvidePossibleElementsAutocomplete(test);
+
+                        var completionWindow = new CompletionWindow(textEditor.TextArea);
+                        IList<ICompletionData> data = completionWindow.CompletionList.CompletionData;
+                        foreach (var autocompleteelement in autocompletelist)
+                        {
+                            data.Add(new MyCompletionData(autocompleteelement));
+                        }
+                        completionWindow.Show();
+                        completionWindow.Closed += delegate
+                        {
+                            completionWindow = null;
+                        };
+
+                        break;
+
                 }
             }
             catch (Exception exc)
             {
-                
+
             }
 
             if (e.Text.Length > 0)
@@ -122,22 +159,97 @@ namespace XmlIntelliSense.App
             }
         }
 
+        public List<string> ProvidePossibleElementsAutocomplete(XmlElementPath path)
+        {
+            List<string> result = new List<string>();
+            XmlSchemaSet schemas = new XmlSchemaSet();
+            schemas.Add("", XmlReader.Create(new StringReader(this.XSD.Text)));
+            LastValidXDocument.Validate(schemas, (o, e) =>
+            {
+            }, true);
+
+            StringBuilder xpath = new StringBuilder();
+            foreach (var element in path.Elements)
+            {
+                xpath.Append("//" + element.Name);
+            }
+
+            var parentElement = LastValidXDocument.XPathSelectElement(xpath.ToString());
+
+            if (parentElement != null)
+            {
+                var element = parentElement.GetSchemaInfo();
+
+                // Get the complex type of the Customer element.
+                XmlSchemaComplexType complexType = element.SchemaType as XmlSchemaComplexType;
+
+                // Get the sequence particle of the complex type.
+                XmlSchemaSequence sequence = complexType.ContentTypeParticle as XmlSchemaSequence;
+
+
+                // Iterate over each XmlSchemaElement in the Items collection.
+                foreach (XmlSchemaObject childElement in sequence.Items)
+                {
+                    var asElement = (XmlSchemaElement)childElement;
+                    result.Add(asElement.Name + " (" + asElement.ElementSchemaType.Name + ")");
+
+                    //result.Add(asElement.Name + " (" + asElement.ElementSchemaType.Name + ")");
+                }
+
+            }
+            return result;
+        }
+
         private void IntelliSense(object sender, EventArgs eventArgs)
         {
-            var GetActiveElementStartPath = XmlParser.GetActiveElementStartPath(textEditor.Text, textEditor.TextArea.Caret.Offset);
-            var GetActiveElementStartPathAtIndex = XmlParser.GetActiveElementStartPathAtIndex(textEditor.Text, textEditor.TextArea.Caret.Offset);
-            var GetAttributeName = XmlParser.GetAttributeName(textEditor.Text, textEditor.TextArea.Caret.Offset);
-            var GetAttributeNameAtIndex = XmlParser.GetAttributeNameAtIndex(textEditor.Text, textEditor.TextArea.Caret.Offset);
-            var GetParentElementPath = XmlParser.GetParentElementPath(textEditor.Text);
+            //var GetActiveElementStartPath = XmlParser.GetActiveElementStartPath(textEditor.Text, textEditor.TextArea.Caret.Offset);
+            //var GetActiveElementStartPathAtIndex = XmlParser.GetActiveElementStartPathAtIndex(textEditor.Text, textEditor.TextArea.Caret.Offset);
+            //var GetAttributeName = XmlParser.GetAttributeName(textEditor.Text, textEditor.TextArea.Caret.Offset);
+            //var GetAttributeNameAtIndex = XmlParser.GetAttributeNameAtIndex(textEditor.Text, textEditor.TextArea.Caret.Offset);
+            //var GetParentElementPath = XmlParser.GetParentElementPath(textEditor.Text);
 
-            StringBuilder builder = new StringBuilder();
-            builder.AppendLine("GetActiveElementStartPath: " + GetActiveElementStartPath.ToString());
-            builder.AppendLine("GetActiveElementStartPathAtIndex: " + GetActiveElementStartPathAtIndex.ToString());
-            builder.AppendLine("GetAttributeName: " + GetAttributeName.ToString());
-            builder.AppendLine("GetAttributeNameAtIndex: " + GetAttributeNameAtIndex.ToString());
-            builder.AppendLine("GetParentElementPath: " + GetParentElementPath.ToString());
+            //StringBuilder builder = new StringBuilder();
+            //builder.AppendLine("GetActiveElementStartPath: " + GetActiveElementStartPath.ToString());
+            //builder.AppendLine("GetActiveElementStartPathAtIndex: " + GetActiveElementStartPathAtIndex.ToString());
+            //builder.AppendLine("GetAttributeName: " + GetAttributeName.ToString());
+            //builder.AppendLine("GetAttributeNameAtIndex: " + GetAttributeNameAtIndex.ToString());
+            //builder.AppendLine("GetParentElementPath: " + GetParentElementPath.ToString());
 
-            this.CurrentPath.Text = builder.ToString();
+            //this.CurrentPath.Text = builder.ToString();
+        }
+    }
+
+    public class MyCompletionData : ICompletionData
+    {
+        public MyCompletionData(string text)
+        {
+            this.Text = text;
+        }
+
+        public System.Windows.Media.ImageSource Image
+        {
+            get { return null; }
+        }
+
+        public string Text { get; private set; }
+
+        // Use this property if you want to show a fancy UIElement in the list.
+        public object Content
+        {
+            get { return this.Text; }
+        }
+
+        public object Description
+        {
+            get { return "Description for " + this.Text; }
+        }
+
+        public double Priority { get; }
+
+        public void Complete(TextArea textArea, ISegment completionSegment,
+            EventArgs insertionRequestEventArgs)
+        {
+            textArea.Document.Replace(completionSegment, this.Text);
         }
     }
 }
