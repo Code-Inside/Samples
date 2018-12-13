62067463d42ca7f6ada3ab09e5aabe6270b065b3
Robert Muehsig <robert.muehsig@codeinside.eu>
2018-12-13 18:01:46 +0100

    office core stuff

---
+++ 2018/officeaddincore/OfficeAppWithDotNetCore.Web/Areas/OfficeAddin/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OfficeAppWithDotNetCore.Web.Areas.OfficeAddin.Controllers
+{
+    [Area("OfficeAddin")]
+    public class HomeController : Controller
+    {
+        public IActionResult TaskPane()
+        {
+            return View();
+        }
+
+        public IActionResult FunctionFile()
+        {
+            return View();
+        }
+    }
+}
---
+++ 2018/officeaddincore/OfficeAppWithDotNetCore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OfficeAppWithDotNetCore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
