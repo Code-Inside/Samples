e614a49736f26b705b6108701412507014beb0c8
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-28 21:49:09 +0800

    Delete 2015 directory

--- 2015/WebApiBasicAuth/WebApiBasicAuth/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebApiBasicAuth.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- 2015/WebApiBasicAuth/WebApiBasicAuth/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiBasicAuth.WebApiApplication" Language="C#" %>
